03a6ff3cb1172c543d10ae48fa13c1621a8563c1
Sammie13 <alwaysextra187@gmail.com>
2020-06-26 23:19:49 -0400

    Updates

---
+++ precache-manifest.7fa4f0f0fea3377d82ce36990391c373.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "380090cf8e957663ef094a78501e78b6",
+    "url": "/sudoku-kudoku-breaker/index.html"
+  },
+  {
+    "revision": "67038a8b2036e9888e1b",
+    "url": "/sudoku-kudoku-breaker/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "3c82d785bda950dbd410",
+    "url": "/sudoku-kudoku-breaker/static/css/main.bcc03939.chunk.css"
+  },
+  {
+    "revision": "67038a8b2036e9888e1b",
+    "url": "/sudoku-kudoku-breaker/static/js/2.97953013.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/sudoku-kudoku-breaker/static/js/2.97953013.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c82d785bda950dbd410",
+    "url": "/sudoku-kudoku-breaker/static/js/main.aa26e362.chunk.js"
+  },
+  {
+    "revision": "9df9bfff5ed1530f663b",
+    "url": "/sudoku-kudoku-breaker/static/js/runtime-main.0ecce489.js"
+  }
+]);
